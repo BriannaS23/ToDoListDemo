d2e1c36a45b5c3f3aafec442cba1c34ce5e0e7cc
Kwk <Kwk>
2023-08-17 22:01:54 -0400

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Brianna Silva on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
